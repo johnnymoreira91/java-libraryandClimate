86749f424c1e8229595568415c30b95e8760af8d
Johnny Moreira <jmoreira@engebras.com.br>
2023-10-08 22:11:14 -0300

    add post authors

--- src/main/java/com/lib/library/Controller/AuthorController.java
+++ src/main/java/com/lib/library/Controller/AuthorController.java
@@ -1,8 +1,11 @@
 package com.lib.library.Controller;
 
+import com.lib.library.features.Author.Author;
 import com.lib.library.features.Author.AuthorRepository;
+import com.lib.library.features.Author.AuthorRequestDTO;
 import com.lib.library.features.Author.AuthorResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +18,25 @@ public class AuthorController {
     private AuthorRepository repository;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author saveAuthor(@RequestBody AuthorRequestDTO data) {
+        try {
+            Author author = new Author(data);
+            return repository.save(author);
+        } catch (Exception e) {
+            throw new RuntimeException("Error ao crier o author: " + e.getMessage(), e);
+        }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<AuthorResponseDTO> getAll() {
         try {
             List<AuthorResponseDTO> authorList = repository.findAll().stream().map(AuthorResponseDTO:: new).toList();
             return authorList;
         } catch (Exception e) {
-            throw e;
+            throw new RuntimeException("Error to list authors:" + e.getMessage(), e);
         }
     }
 }
